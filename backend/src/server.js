91e908dab9532dd66f82ec806bab77944b4f01e1
VinhGH <github.for.vinh@gmail.com>
2026-02-02 20:21:30 +0700

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: "https://authencation-screen.vercel.app" || 'http://localhost:5173',
     credentials: true
 }));
 
